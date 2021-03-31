bc2b1e69eae92fcd4dd9809361a9bb245e16b3dc
jocantu <jocantu@protonmail.com>
2021-03-31 00:25:04 -0600

    Working API

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
+
+app.use(cors())
+
+let rappers = {
+  '21 savage':{
+    age: 30,
+    birthName: 'Joel Cantu',
+    birthLocation: 'Torreón, México'
+  },
+  'cartel de santa':{
+    age: 45,
+    birthName: 'Hecot Montaño',
+    birthLocation: 'Guadalajara, México'
+  },
+  'el gran silencio':{
+    age: 52,
+    birthName: 'Carlos Lozada',
+    birthLocation: 'CDMX, México'
+  },
+  'unknown':{
+    age: 999,
+    birthName: 'unknown',
+    birthLocation: 'unknown'
+  }
+}
+
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/rappers/:rapperName', (request, response) => {
+  const rapName = request.params.rapperName.toLowerCase()
+  console.log(rapName)
+  if(rappers[rapName]){
+    response.json(rappers[rapName])
+  } else {
+    response.json(rappers['unknown'])
+  }
+})
+
+app.listen(PORT, ()=>{
+  console.log(`Server running on port ${PORT}`)
+})
